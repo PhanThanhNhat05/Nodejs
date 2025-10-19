1e696943c6e8f9f50aa012c1247727565b8bc0da
Your Name <you@example.com>
2025-10-20 00:13:46 +0700

    data

--- product-management/controllers/admin/productController.js
+++ product-management/controllers/admin/productController.js
@@ -1,8 +1,13 @@
 //[GET] admin/products
-module.exports.index = (req, res) => {
-  const sampleProducts = [
-    { _id: '1', title: 'Sản phẩm 1', price: 100, status: 'active', thumbnail: '/images/ip.jpg' },
-    { _id: '2', title: 'Sản phẩm 2', price: 150, status: 'inactive', thumbnail: '/images/ip.jpg' }
-  ];
-  res.render('admin/pages/products/index', { pageTitle: 'Quản Lý Sản Phẩm', products: sampleProducts });
+const Product = require('../../models/productModeDB');
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);
+//   const sampleProducts = [
+//     { _id: '1', title: 'Sản phẩm 1', price: 100, status: 'active', thumbnail: '/images/ip.jpg' },
+//     { _id: '2', title: 'Sản phẩm 2', price: 150, status: 'inactive', thumbnail: '/images/ip.jpg' }
+//   ];
+  res.render('admin/pages/products/index', { pageTitle: 'Quản Lý Sản Phẩm', products: products });
 }
