59d339138e49d816018172d2c8a09616e69c67a4
Your Name <you@example.com>
2025-10-19 23:54:22 +0700

    giao diện danh sách sản phẩm

---
+++ product-management/controllers/admin/productController.js
@@ -0,0 +1,7 @@
+module.exports.index = (req, res) => {
+  const sampleProducts = [
+    { _id: '1', title: 'Sản phẩm 1', price: 100, status: 'active', thumbnail: '/images/ip.jpg' },
+    { _id: '2', title: 'Sản phẩm 2', price: 150, status: 'inactive', thumbnail: '/images/ip.jpg' }
+  ];
+  res.render('admin/pages/products/index', { pageTitle: 'Quản Lý Sản Phẩm', products: sampleProducts });
+}
--- product-management/routes/admin/indexroute.js
+++ product-management/routes/admin/indexroute.js
@@ -1,10 +1,11 @@
 const systemConfig = require('../../config/system');
-const  dashboardRoute  = require('./dashboardroute');
-
+const dashboardRoute  = require('./dashboardroute');
+const productRoute = require('./productRouter');
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
+  app.use(`${PATH_ADMIN}/products`, productRoute);
 
 }
 
