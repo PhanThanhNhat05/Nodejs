fc41aaf08d34ebc4d4dddbb67cea2ba6b8746029
Your Name <you@example.com>
2025-10-20 23:40:07 +0700

    tối ưu search dễ tái sử dụng và sửa

--- product-management/controllers/admin/productController.js
+++ product-management/controllers/admin/productController.js
@@ -1,34 +1,37 @@
 //[GET] admin/products
 const Product = require('../../models/productModeDB');
 const filterStatusHelper = require('../../helpers/filterStatus');
+const searchgHelper = require('../../helpers/search');
 module.exports.index = async (req, res) => {
-    console.log(req.query.status)
+  console.log(req.query.status)
 
-    //tinh nang loc status
-    const filterStatus =  filterStatusHelper(req.query);
-    console.log(filterStatus);
-    
-    let find = {
-        deleted: false, 
-    }
-    if (req.query.status) {
-        find.status = req.query.status
-    }
+  //tinh nang loc status
+  const filterStatus = filterStatusHelper(req.query);
+  console.log(filterStatus);
 
-   //tinh nang tim kiem
-    let keyword = "";
-    if(req.query.keyword){
-      keyword = req.query.keyword;
-      const regex = new RegExp(keyword, 'i');
-      find.title = regex;
-    }
-    const products = await Product.find({...find});
-    // console.log(products);
+  let find = {
+    deleted: false,
+  }
+  if (req.query.status) {
+    find.status = req.query.status
+  }
 
-  res.render('admin/pages/products/index', { 
-    pageTitle: 'Quản Lý Sản Phẩm', 
-    products: products, 
-    filterStatus: filterStatus ,
-    keyword: keyword
+  //tinh nang tim kiem
+  const objSearch = searchgHelper(req.query);
+  console.log(objSearch);
+
+  if (objSearch.regex) {
+    find.title = objSearch.regex;
+  }
+  const products = await Product.find({
+    ...find
+  });
+  // console.log(products);
+
+  res.render('admin/pages/products/index', {
+    pageTitle: 'Quản Lý Sản Phẩm',
+    products: products,
+    filterStatus: filterStatus,
+    keyword: objSearch.keyword
   });
 }
