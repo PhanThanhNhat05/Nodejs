4d65a750c7a816ba804e5c9313935a11780aaae1
Your Name <you@example.com>
2025-10-24 18:05:14 +0700

    xử lí uploads ảnh từ máy tính . nhưng chưa preview được hình ảnh khi đẩy lên

--- product-management/controllers/admin/productController.js
+++ product-management/controllers/admin/productController.js
@@ -126,7 +126,7 @@ module.exports.create = async (req, res) => {
 //[POST] admin/products/create
 
 module.exports.createPost = async (req, res) => {
-
+  console.log(req.file)
   req.body.price = parseInt(req.body.price)
   req.body.discountPercentage = parseInt(req.body.discountPercentage)
   req.body.stock = parseInt(req.body.stock)
@@ -137,7 +137,8 @@ module.exports.createPost = async (req, res) => {
   else {
     req.body.position = parseInt(req.body.position)
   }
-  console.log(req.body)
+  req.body.thumbnail = `/uploads/${req.file.filename}`
+  // console.log(req.body)
   const product = new Product(req.body);
   await product.save();
 
--- product-management/routes/admin/productRouter.js
+++ product-management/routes/admin/productRouter.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer  = require('multer')
+const storageMulter = require("../../helpers/storage-multer")
+const upload = multer({ storage: storageMulter() })
 const router = express.Router();
 const controller = require("../../controllers/admin/productController");
 
@@ -7,6 +10,6 @@ router.patch('/change-status/:status/:id', controller.changeStatus);
 router.patch('/change-multi', controller.changeMulti)
 router.delete('/delete/:id', controller.deleteItem)
 router.get('/create', controller.create)
-router.post('/create', controller.createPost)
+router.post('/create',upload.single('thumbnail'), controller.createPost)
 
 module.exports = router;
