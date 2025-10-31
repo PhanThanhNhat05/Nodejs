24206ad3dfcd5ce4157e907b2486d9c866a05299
Your Name <you@example.com>
2025-10-31 22:33:06 +0700

    fix lỗi STT

--- product-management/helpers/createTree.js
+++ product-management/helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;
  function createTree(arr, parentId="") {
     const tree = [];
     arr.forEach((item) => {
       if(item.parent_id === parentId) {
+        count++;
         const newItem = item;
+        newItem.index = count;
         const children = createTree(arr, item.id) ;
         if(children.length > 0) {
           newItem.children = children;
@@ -13,6 +16,7 @@
     return tree;
 }
 module.exports.Tree = (arr, parentId="") => {
+    count = 0
     const tree = createTree(arr, parentId="")
     
     return tree;
