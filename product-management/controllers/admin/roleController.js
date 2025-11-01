7ce103947f2374882953a3baf5b125d47fed13f3
Your Name <you@example.com>
2025-11-02 01:38:12 +0700

    xây dựng phần chỉnh sửa nhóm quyền

--- product-management/controllers/admin/roleController.js
+++ product-management/controllers/admin/roleController.js
@@ -29,3 +29,42 @@ module.exports.createPost = async (req, res) => {
     await records.save();
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
+
+//[GET] admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+    let find = {
+        _id : id,
+        deleted: false
+    }
+
+    const data = await Role.findOne(find)
+    console.log(data)
+    res.render("admin/pages/roles/edit", { 
+        pageTitle: "Sửa nhóm quyền" ,
+        data : data
+    });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`)
+    }
+    
+}
+
+//[PATCH] admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+    
+
+    await Role.updateOne({_id: id}, req.body);
+    req.flash("success", "Cập nhật thành công")
+     
+    
+    } catch (error) {
+        req.flash("error", "Cập nhật thất bại")
+    }
+    
+    // res.redirect(`${systemConfig.prefixAdmin}/roles/edit/:id`)
+    res.redirect("back")
+}
